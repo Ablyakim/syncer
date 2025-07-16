dfaf278814fccd18bfcd9d0dd9f19cd42871b10d
Ablyakim Ablyalimov <itioprog@gmail.com>
2025-07-16 21:37:45 +0300

    handle connection closing, add mutex

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -19,22 +20,34 @@ var upgrader = websocket.Upgrader{
 
 var hub Hub
 
+type Config struct {
+	Addr string
+	Path string
+}
+
 type ConnInfo struct {
 	ID   string
 	Conn *websocket.Conn
 }
 
-var connections = map[string]ConnInfo{}
-
 type Hub struct {
 	connections map[string]ConnInfo
-	// Add mutex
+	mu          sync.RWMutex
 }
 
 func (h *Hub) RegisterConnection(c ConnInfo) {
 	if _, ok := h.connections[c.ID]; !ok {
+		h.mu.Lock()
 		h.connections[c.ID] = c
+		h.mu.Unlock()
 	}
+}
+
+func (h *Hub) RemoveConnection(c ConnInfo) {
+	log.Printf("Remove connection %s from hub", c.ID)
+	h.mu.Lock()
+	delete(h.connections, c.ID)
+	h.mu.Unlock()
 }
 
 func (h *Hub) Listen(c ConnInfo) {
@@ -47,10 +60,6 @@ func (h *Hub) Listen(c ConnInfo) {
 		log.Printf("recv: %s from ID=%s", message, c.ID)
 		h.Broadcast(c.ID, mt, message)
 	}
-}
-
-func (h *Hub) UnRegisterConnection(c ConnInfo) {
-	panic("Implement me")
 }
 
 func (h *Hub) WriteMessage(connID string, mt int, msg []byte) error {
@@ -91,6 +100,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	connID := uuid.NewString()
 	ci := ConnInfo{ID: connID, Conn: c}
 	hub.RegisterConnection(ci)
+	defer hub.RemoveConnection(ci)
 
 	err = hub.WriteMessage(connID, websocket.TextMessage, currTheme)
 	if err != nil {
